889b5ab6d015e16d405a6d136f5e90b13f7e5db5
ShintaroNippon <eu@exemplo.com>
2020-10-06 20:19:36 +0100

    update zero found

---
+++ src/app/components/ZeroFound/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const ZeroFound = (): JSX.Element => {
+  return (
+    <div className="mt50 col-md-8 offset-md-2">
+      <div className="mt50 zero-found col-md-4 offset-md-4"></div>
+      <div className="mt50 text-center">Humm ... We didn`t find any users...</div>
+    </div>
+  );
+};
